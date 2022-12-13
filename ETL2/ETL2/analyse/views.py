9b8b1a99dbc3648db29b1ee91f5af4a16988af19
Saia Ralandison <ralandison.saia@gmail.com>
2022-12-13 12:01:29 +0400

    import fait
    import fait, on peut maintenant importer les fichiers,
    prochain étape les enregistrer dans la BDD

--- ETL2/ETL2/analyse/views.py
+++ ETL2/ETL2/analyse/views.py
@@ -10,6 +10,8 @@ from .forms import UploadFileForm
 from django import forms
 from .handle_uploaded_file import handle_uploaded_file
 import pandas as pd
+from csv import DictReader
+from io import TextIOWrapper
 
 # Create your views here.
 @login_required (login_url='login')
@@ -24,13 +26,14 @@ def importF (request):
         print(data)
         print("--------------------")
         # import pdb; pdb.set_trace()
-        data1 = data.read().decode('ISO-8859-1')
+        file1 = TextIOWrapper(data, encoding="ISO-8859-1", newline="")
         print("--------------------")
-        df = pd.read_csv(data1, delimiter=",")
+        df = pd.read_csv(file1, delimiter=",")
+        infos1 = df.head ()
         context = {
             'n1': 2000 ,
             'n2': 3000 ,
-            # 'n3' :infos1,
+            'n3' :infos1,
             }
         template = loader.get_template('retourImp.html')
         return HttpResponse(template.render(context, request)) 
